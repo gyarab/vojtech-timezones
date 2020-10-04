a757f33e77655bf0d51fcb41de13f6b0250c6057
VV <vojtech.vojtech@student.gyarab.cz>
2020-10-04 17:50:04 +0200

    Ukol

---
+++ muj-web/src/main/java/ServletTime.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.DateTimeException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+/**
+ * @author 2017-e-vojtech
+ */
+@WebServlet(urlPatterns = {"/time"})
+public class ServletTime extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String timeZoneFromUser = request.getParameter("timezoneuser");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Time in zone entered by user</title>");
+            out.println("</head>");
+            out.println("<body>");
+            try {
+                ZoneId zone = ZoneId.of(timeZoneFromUser);
+                ZonedDateTime now = ZonedDateTime.now(zone);
+                out.println("<h1>Ve vámi zadaném pásmu je tento čas " + now + "</h1>");
+            } catch (DateTimeException e) {
+                out.println("<h1>Něco bylo špatně se zadanou časovou zónou</h1>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ muj-web/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>TimeZone from user</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<form name="zoneForm" method="post" action="time">
+    Zadejte název časové zóny:<input type="text" name="timezoneuser"/> <br/>
+    <input type="submit" value="Jaký je tam čas?"/>
+</form>
+</body>
+</html>
